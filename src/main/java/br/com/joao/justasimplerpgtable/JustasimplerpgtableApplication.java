998730ca3897853b1e0dab49503c04db9a347cec
JoaoEmanoelConinch <jecconinch@gmail.com>
2021-12-14 20:49:01 -0300

    criando projeto spring

---
+++ src/main/java/br/com/joao/justasimplerpgtable/JustasimplerpgtableApplication.java
@@ -0,0 +1,13 @@
+package br.com.joao.justasimplerpgtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustasimplerpgtableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustasimplerpgtableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joao/justasimplerpgtable/JustasimplerpgtableApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joao.justasimplerpgtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustasimplerpgtableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
