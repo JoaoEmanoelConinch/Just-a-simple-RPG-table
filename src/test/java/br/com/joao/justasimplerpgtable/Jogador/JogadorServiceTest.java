2a252942610b35a8e74491a41d1010310161e4a3
JoaoEmanoelConinch <jecconinch@gmail.com>
2021-12-31 10:51:37 -0300

    criando test para jogador service

--- src/test/java/br/com/joao/justasimplerpgtable/Jogador/JogadorServiceTest.java
+++ src/test/java/br/com/joao/justasimplerpgtable/Jogador/JogadorServiceTest.java
@@ -1,57 +1,5 @@
 package br.com.joao.justasimplerpgtable.Jogador;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import br.com.joao.justasimplerpgtable.model.entity.JogadorEntity;
-import br.com.joao.justasimplerpgtable.model.entity.PersonagemEntity;
-import br.com.joao.justasimplerpgtable.model.repository.JogadorRepo;
-
-
-@DataJpaTest
 public class JogadorServiceTest {
     
-    @Autowired
-    private JogadorRepo jogadorRepo;
-
-    @AfterEach
-    void afterEach(){
-        jogadorRepo.deleteAll();
-    }
-
-    @Test
-    void deveBoscarUmJogadorporNome(){
-        //given
-        JogadorEntity jogador = new JogadorEntity(
-            "e",
-            "e",
-            new ArrayList<PersonagemEntity>(),
-            true);
-            jogadorRepo.save(jogador);
-
-        //when
-        List<JogadorEntity> jogadorEntityDB = jogadorRepo.findJogadorEntityByAtivoTrueAndNomeContaining("e");
-
-        //then
-        assertFalse(jogadorEntityDB.isEmpty());
-    }
-
-    @Test
-    void deveBoscarUmJogadorporNomeEDarErro(){
-        //when
-        List<JogadorEntity> jogadorEntityDB = jogadorRepo.findJogadorEntityByAtivoTrueAndNomeContaining ("e");
-
-        //then
-        assertTrue(jogadorEntityDB.isEmpty());
-    }
-
 }
