71ba2e84c967913fa012da56cecef86d30c4f533
JoaoEmanoelConinch <jecconinch@gmail.com>
2022-01-03 09:47:35 -0300

    removendo imports desnecessarios

--- src/main/java/br/com/joao/justasimplerpgtable/model/entity/JogadorEntity.java
+++ src/main/java/br/com/joao/justasimplerpgtable/model/entity/JogadorEntity.java
@@ -6,16 +6,12 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import br.com.joao.justasimplerpgtable.model.enumeratiom.StatusJogador;
 
 /**
  * @author JoaoEmanoelConinch
@@ -139,4 +135,9 @@ public class JogadorEntity implements Serializable {
 		return "JogadorEntity [nome=" + nome +"]";
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
 }
