d27f0e5aecf3dd3e3e54a81116cd8559eff0c5b6
JoaoEmanoelConinch <jecconinch@gmail.com>
2022-01-03 21:04:43 -0300

    criando os tests de jogador repo

--- src/main/java/br/com/joao/justasimplerpgtable/JustasimplerpgtableApplication.java
+++ src/main/java/br/com/joao/justasimplerpgtable/JustasimplerpgtableApplication.java
@@ -1,7 +1,14 @@
 package br.com.joao.justasimplerpgtable;
 
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.com.joao.justasimplerpgtable.model.entity.JogadorEntity;
+import br.com.joao.justasimplerpgtable.model.service.*;
 
 
 @SpringBootApplication
@@ -11,13 +18,14 @@ public class JustasimplerpgtableApplication {
 		SpringApplication.run(JustasimplerpgtableApplication.class, args);
 	}
 
-	// @Bean
-	// CommandLineRunner commandLineRunner (JogadorService jogadorService) {
-	// 	return args -> {
-	// 		System.out.println();
-	// 		System.out.println(jogadorService.getAllByNome("Joao").get(0).getNome());
-	// 		System.out.println();
-	// 	};
-	// }
+	@Bean
+	CommandLineRunner commandLineRunner (JogadorService jogadorService) {
+		return args -> {
+			System.out.println();
+			List<JogadorEntity> jogadorEntities = jogadorService.getAll();
+			jogadorEntities.stream().forEach(e -> System.out.println(e.getAtivo()));
+			System.out.println();
+		};
+	}
 	
 }
--- src/test/java/br/com/joao/justasimplerpgtable/Jogador/JogadorRepoTest.java
+++ src/test/java/br/com/joao/justasimplerpgtable/Jogador/JogadorRepoTest.java
@@ -1,23 +1,24 @@
 package br.com.joao.justasimplerpgtable.Jogador;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import br.com.joao.justasimplerpgtable.model.entity.JogadorEntity;
 import br.com.joao.justasimplerpgtable.model.entity.PersonagemEntity;
 import br.com.joao.justasimplerpgtable.model.repository.JogadorRepo;
 
 
-@DataJpaTest
+@SpringBootTest
 public class JogadorRepoTest {
     
     @Autowired
@@ -29,6 +30,39 @@ public class JogadorRepoTest {
     }
 
     @Test
+    void deveRecuperarOJogadorPorId(){
+        //given
+        JogadorEntity jogador = new JogadorEntity(
+            "a",
+            "a",
+            new ArrayList<PersonagemEntity>(),
+            true);
+        JogadorEntity jogadorEntity = jogadorRepo.save(jogador);
+        //wher
+        Optional<JogadorEntity> newJogadorEntity = jogadorRepo.findJogadorEntityByIdAndAtivoTrue(jogadorEntity.getId());
+
+        //then
+        assertEquals(newJogadorEntity.get(),jogadorEntity);
+
+    }
+
+    @Test
+    void naoDeveRecuperarOJogadorPorIdSeOJogadorEstiverInativo(){
+        //given
+        JogadorEntity jogador = new JogadorEntity(
+            "e",
+            "e",
+            new ArrayList<PersonagemEntity>(),
+            false);
+        JogadorEntity jogadorEntity = jogadorRepo.save(jogador);
+        //wher
+        Optional<JogadorEntity> newJogadorEntity = jogadorRepo.findJogadorEntityByIdAndAtivoTrue(jogadorEntity.getId());
+
+        //then
+        assertTrue(newJogadorEntity.isEmpty());
+    }
+
+    @Test
     void deveBoscarUmJogadorporNome(){
         //given
         JogadorEntity jogador = new JogadorEntity(
@@ -36,7 +70,7 @@ public class JogadorRepoTest {
             "e",
             new ArrayList<PersonagemEntity>(),
             true);
-            jogadorRepo.save(jogador);
+        jogadorRepo.save(jogador);
 
         //when
         List<JogadorEntity> jogadorEntityDB = jogadorRepo.findJogadorEntityByAtivoTrueAndNomeContaining("e");
@@ -46,12 +80,57 @@ public class JogadorRepoTest {
     }
 
     @Test
+    void naoDeveBoscarUmJogadorporNomeSeEle(){
+        //given
+        JogadorEntity jogador = new JogadorEntity(
+            "e",
+            "e",
+            new ArrayList<PersonagemEntity>(),
+            false);
+            jogadorRepo.save(jogador);
+
+        //when
+        List<JogadorEntity> jogadorEntityDB = jogadorRepo.findJogadorEntityByAtivoTrueAndNomeContaining("e");
+
+        //then
+        assertTrue(jogadorEntityDB.isEmpty());
+    }
+
+    @Test
     void deveBoscarUmJogadorporNomeEDarErro(){
         //when
         List<JogadorEntity> jogadorEntityDB = jogadorRepo.findJogadorEntityByAtivoTrueAndNomeContaining ("e");
 
         //then
         assertTrue(jogadorEntityDB.isEmpty());
+    }
+
+    @Test
+    void deveRecuperarTodosOsJogadoresAtivos(){
+        JogadorEntity jogador1 = new JogadorEntity(
+            "e",
+            "e",
+            new ArrayList<PersonagemEntity>(),
+            true);
+        jogadorRepo.save(jogador1);
+
+        JogadorEntity jogador2 = new JogadorEntity(
+            "2",
+            "e2",
+            new ArrayList<PersonagemEntity>(),
+            true);
+        jogadorRepo.save(jogador2);
+
+        JogadorEntity jogador3 = new JogadorEntity(
+            "ee",
+            "eqw",
+            new ArrayList<PersonagemEntity>(),
+            true);
+        jogadorRepo.save(jogador3);
+
+        List<JogadorEntity> jogadorEntities = jogadorRepo.findJogadorEntityByAtivoTrue();
+
+        assertEquals(3, jogadorEntities.size());
     }
 
 }
